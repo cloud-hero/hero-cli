6aab0be97566dc68c874b071d65ddfc23aeb1af7
CloudHero <hero@cloudhero.io>
2016-04-04 17:42:21 +0300

    Added ssh and caching.

--- pyhero.py
+++ pyhero.py
@@ -1,10 +1,13 @@
+from collections import defaultdict
 import json
 import requests
 
-from utils import remove_none_dict_values
+from constants import CLOUD_HERO_NODES_CACHE, CLOUD_HERO_ENVIRONMENTS_CACHE
+from utils import remove_none_dict_values, cache_to_file, invalidate_cache
 
 DEFAULT_CLIENT_VERSION = '1.0'
 DEFAULT_TIMEOUT_SECONDS = 60
+CLIENT_CACHE_TTL = 60 * 5
 
 class NotFound(Exception): pass
 class APIError(Exception): pass
@@ -13,12 +16,14 @@ class APIError(Exception): pass
 ENDPOINTS = {
     'register': '/accounts/register',
     'login': '/accounts/tokens',
+    'account_keys': '/accounts/keys?private=True',
     'providers': '/providers',
     'integrations': '/integrations',
     'nodes': '/environments/{}/nodes',
     'nodes_delete': '/nodes',
     'nodes_scale': '/environments/{}/scale',
     'environments': '/environments',
+    'applications': '/applications',
     'swarm': '/swarm',
 }
 
@@ -71,6 +76,9 @@ class Client(object):
     def delete_provider(self, item_id):
         return self._result(self.delete_item(ENDPOINTS['providers'], item_id))
 
+    def list_key(self):
+        return self._result(self.get(ENDPOINTS['account_keys']))
+
     def show_integration_types(self):
         return self._result(self.options(ENDPOINTS['integrations']))
 
@@ -85,10 +93,12 @@ class Client(object):
         return self._result(self.delete_item(ENDPOINTS['integrations'],
                                              item_id))
 
+    @invalidate_cache(CLOUD_HERO_ENVIRONMENTS_CACHE, CLOUD_HERO_NODES_CACHE)
     def create_environment(self, data):
         return self._result(self.post_json(ENDPOINTS['environments'],
                                            data=data))
 
+    @cache_to_file(CLOUD_HERO_ENVIRONMENTS_CACHE, ttl=CLIENT_CACHE_TTL)
     def list_environments(self):
         return self._result(self.get(ENDPOINTS['environments']))
 
@@ -97,22 +107,60 @@ class Client(object):
         return self._result(self.delete_item(ENDPOINTS['environments'], item_id,
                                              **kwargs))
 
+    @invalidate_cache(CLOUD_HERO_ENVIRONMENTS_CACHE, CLOUD_HERO_NODES_CACHE)
     def create_node(self, environment_id, data):
         return self._result(self.post_json(self._nodes_endpoint(environment_id),
                                            data=data))
 
-    def list_nodes(self, environment_id):
-        return self._result(self.get(self._nodes_endpoint(environment_id)))
+    @cache_to_file(CLOUD_HERO_NODES_CACHE, ttl=CLIENT_CACHE_TTL)
+    def get_all_details(self):
+        applications = self._result(self.get(ENDPOINTS['applications']))
+        nodes_data = defaultdict(list)
+        for application in applications:
+            for environment in application['environments']:
+                for node in environment['nodes']:
+                    node_name = node['name']
+                    node_data = {
+                        'node': {
+                            'id': node['id'],
+                            'name': node['name'],
+                            'private_ip': node['private_ip'],
+                            'public_ip': node['public_ip'],
+                            'packages': node['packages'],
+                            'size': node['size'],
+                            'tags': node['tags']
+                        },
+                        'application': {
+                            'name': application['application_name'],
+                            'id': application['id'],
+                        },
+                        'environment': {
+                            'name': environment['name'],
+                            'id': environment['id'],
+                            'location': environment['os_region']
+                        },
+                        'provider': {
+                            'id': environment['provider']['id'],
+                            'name': environment['provider']['name'],
+                            'type': environment['provider']['provider_type']
+                        }
+                    }
+                    nodes_data[node_name].append(node_data)
+        return nodes_data
 
+    @invalidate_cache(CLOUD_HERO_ENVIRONMENTS_CACHE, CLOUD_HERO_NODES_CACHE)
     def delete_node(self, item_id, **params):
         kwargs = {'params': params}
         return self._result(self.delete_item(ENDPOINTS['nodes_delete'], item_id,
                                              **kwargs))
 
+    @invalidate_cache(CLOUD_HERO_ENVIRONMENTS_CACHE, CLOUD_HERO_NODES_CACHE)
     def scale_up_node(self, environment_id, data):
+        from ipdb import set_trace; set_trace()
         result = self.post_json(self._scale_endpoint(environment_id), data=data)
         return self._result(result)
 
+    @invalidate_cache(CLOUD_HERO_ENVIRONMENTS_CACHE, CLOUD_HERO_NODES_CACHE)
     def scale_down_node(self, environment_id, data):
         result = self.delete_json(self._scale_endpoint(environment_id), data)
         return self._result(result)
--- utils.py
+++ utils.py
@@ -1,3 +1,91 @@
+import errno
+import functools
+import json
+import os
+import time
+
+
+def write_to_file(content, file_path, is_json=False):
+    expanded_file_path = os.path.expanduser(file_path)
+    dirname = os.path.dirname(expanded_file_path)
+    if not os.path.exists(dirname):
+        try:
+            os.makedirs(dirname)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+
+    with open(expanded_file_path, 'w') as file_handler:
+        if is_json:
+            json.dump(content, file_handler)
+        else:
+            file_handler.write(content)
+
+
+def read_from_file(file_path, is_json=False):
+    expanded_file_path = os.path.expanduser(file_path)
+    try:
+        with open(expanded_file_path, 'r') as file_handler:
+            if is_json:
+                return json.load(file_handler)
+            return file_handler.read()
+    except IOError as exception:
+        if exception.errno == 2:
+            return None
+        raise
+
+
+def cache_to_file(file_path, ttl=60):
+    def wrap(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # Try reading from cache file, if it was created within the TTL.
+            expanded_file_path = os.path.expanduser(file_path)
+            cached = read_cache_file(expanded_file_path, ttl)
+            if cached:
+                return cached
+
+            # Request data.
+            result = func(*args, **kwargs)
+
+            # Write data to cache file.
+            write_to_file(result, expanded_file_path, is_json=True)
+
+            return result
+
+        return wrapper
+
+    return wrap
+
+
+def invalidate_cache(*file_paths):
+    def wrap(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for file_path in file_paths:
+                expanded_file_path = os.path.expanduser(file_path)
+                try:
+                    os.remove(expanded_file_path)
+                except OSError as exc:
+                    if exc.errno == errno.ENOENT:
+                        continue
+                    raise
+            return func(*args, **kwargs)
+        return wrapper
+    return wrap
+
+
+def read_cache_file(file_path, ttl):
+    if not os.path.exists(file_path):
+        return None
+    file_stats = os.stat(file_path)
+    creation_time = file_stats.st_ctime
+    if time.time() - creation_time < float(ttl):
+        cached = read_from_file(file_path, is_json=True)
+        return cached
+    return None
+
+
 def remove_none_dict_values(obj):
     """
     Remove None values from dict.
@@ -10,3 +98,10 @@ def remove_none_dict_values(obj):
                          if v is not None)
     else:
         return obj
+
+
+def set_keys_to_empty_values(obj):
+    if isinstance(obj, dict):
+        return dict((k, set_keys_to_empty_values(v)) for k, v in obj.items())
+    else:
+        return ''
