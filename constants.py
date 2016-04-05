9a698bb7e424efb78e753f82ae31aa93d9393c2c
CloudHero <hero@cloudhero.io>
2016-04-05 17:34:16 +0300

    Updated to staging env

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ VERBOSE = False
 CLI_VERSION = '0.1'
 SSH_KEY_PATH = '~/.ssh/id_rsa_cloudhero'
 
-CLOUD_HERO_URI = 'http://s.cloudhero.io'
+CLOUD_HERO_URI = 'http://d.cloudhero.io'
 CLOUD_HERO_TOKEN_ENV_VARIABLE = 'CLOUD_HERO_TOKEN'
 
 CLOUD_HERO_DIR = '~/.herorc'
