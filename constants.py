2597d18ee6072763935ea4047cdce75f561cc52a
CloudHero <hero@cloudhero.io>
2016-04-09 12:41:22 +0300

    At logout, remove everything from the cloud_hero directory.

--- constants.py
+++ constants.py
@@ -1,12 +1,15 @@
 VERBOSE = False
 CLI_VERSION = '0.1'
-SSH_KEY_PATH = '~/.ssh/id_rsa_cloudhero'
 
+# API-related constants.
+# https://docs.cloudhero.io
 CLOUD_HERO_URI = 'http://d.cloudhero.io'
 CLOUD_HERO_TOKEN_ENV_VARIABLE = 'CLOUD_HERO_TOKEN'
 
-CLOUD_HERO_DIR = '~/.herorc'
-CLOUD_HERO_TOKEN = CLOUD_HERO_DIR + '/token'
-CLOUD_HERO_CACHE_NODES = CLOUD_HERO_DIR + '/cache/nodes'
-CLOUD_HERO_CACHE_ENVIRONMENTS = CLOUD_HERO_DIR + '/cache/enviornments'
-CLOUD_HERO_CACHE_OPTIONS = CLOUD_HERO_DIR + '/cache/options'
+# Local directory-related constants.
+CLOUD_HERO_DIR = '~/.herorc/'
+CLOUD_HERO_SSH_KEY = CLOUD_HERO_DIR + '.ssh/id_rsa_cloudhero'
+CLOUD_HERO_TOKEN = CLOUD_HERO_DIR + 'token'
+CLOUD_HERO_CACHE_NODES = CLOUD_HERO_DIR + 'cache/nodes'
+CLOUD_HERO_CACHE_ENVIRONMENTS = CLOUD_HERO_DIR + 'cache/enviornments'
+CLOUD_HERO_CACHE_OPTIONS = CLOUD_HERO_DIR + 'cache/options'
