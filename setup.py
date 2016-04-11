8291df7602d381cf836ef6bd43a1525b6108edfc
CloudHero <hero@cloudhero.io>
2016-04-11 12:54:42 +0300

    Updated CLI the pypi version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from pip.req import parse_requirements
 
-from hero.constants import CLI_VERSION
+from lib.constants import CLI_VERSION
 
 DOWNLOAD_URL = ('https://github.com/cloud-hero/hero-cli/tarball/{version}'
                 .format(version=CLI_VERSION))
@@ -10,32 +10,35 @@ LONG_DESCRIPTION = ('Use hero to create any type of server environments or '
                     'private cloud providers.')
 
 required_packages = [str(package.req)
-                     for package in parse_requirements('hero/requirements.txt',
+                     for package in parse_requirements('requirements.txt',
                                                        session=False)]
 
 
 setup(
+    # Basic metadata
     name='hero',
-    packages=['hero'],
-
     version=CLI_VERSION,
-    url='https://github.com/cloud-hero/hero-cli',
-    description='Simple server environments set-up.',
-    long_description=LONG_DESCRIPTION,
-
     author='CloudHero',
     author_email='founders@cloudhero.io',
+    url='https://github.com/cloud-hero/hero-cli',
+
+    # Additional information
+    description='Simple server environments set-up.',
+    long_description=LONG_DESCRIPTION,
     license='GNU General Public License',
 
-    install_requires=required_packages,
+    # How to do the install
     download_url=DOWNLOAD_URL,
-    keywords=['CloudHero CLI', 'software-defined infrastructure',
-              'Server set-up'],
-    entry_points={
-        'console_scripts':
-            ['hero = hero.hero:hero_cli']
-    },
+    install_requires=required_packages,
+    packages=['lib'],
+    scripts=[
+        'hero',
+    ],
 
+    # How to do the tests
+    # TODO
+
+    # PyPI metadata.
     # Entire list here:
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -50,4 +53,6 @@ setup(
         'Topic :: System :: Installation/Setup',
         'Topic :: System :: Shells'
     ],
+    keywords=['CloudHero CLI', 'software-defined infrastructure',
+              'Server set-up'],
 )
